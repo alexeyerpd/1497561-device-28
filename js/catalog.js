b36bf38d86cfb07b684124eebf395832b3c87bd3
alexeyerpd <alexeyerpd@gmail.com>
2020-06-21 22:16:24 +0300

    Оживляем интерактивные элементы
    add animation
    
    fix comments
    
    add services slider
    
    fix index.js

---
+++ js/catalog.js
@@ -0,0 +1,146 @@
+const left = document.querySelector('.range__left');
+const right = document.querySelector('.range__right');
+
+const outputLeft = document.querySelector('.range__output-left');
+const outputRight = document.querySelector('.range__output-right');
+
+const indicator = document.querySelector('.range__indicator');
+
+const track = document.querySelector('.range__track');
+
+const trackRect = track.getBoundingClientRect();
+
+const activeList = [];
+
+
+class RangeSlider {
+    constructor(range, track, sliderLeft, sliderRight) {
+        this.range = range;
+        this.track = track;
+        this.sliderLeft = sliderLeft;
+        this.sliderRight = sliderRight;
+
+        this.trackLength = 0;
+        this.stepLength = 0;
+    }
+
+    init() {
+        this.trackLength = trackRect.width - this.sliderLeft.boundingData.width * 2;
+        this.stepLength = this.range / this.trackLength;
+
+        this.update();
+    }
+
+    update() {
+        const from = Math.floor(this.stepLength * this.sliderLeft.currentPosition);
+        const to = Math.floor((this.trackLength - this.sliderRight.currentPosition) * this.stepLength);
+
+        this.updateIndicator();
+
+        outputLeft.textContent = `от ${from}`;
+        outputRight.textContent = `до ${to}`;
+    }
+
+    updateIndicator() {
+        const indicatorPositionLeft = this.sliderLeft.currentPosition + this.sliderLeft.boundingData.width / 2;
+        const indicatorPositionRight = this.sliderRight.currentPosition + this.sliderRight.boundingData.width / 2;
+
+        indicator.style.left = `${indicatorPositionLeft}px`;
+        indicator.style.right = `${indicatorPositionRight}px`;
+    }
+
+}
+
+class Slider {
+    constructor(element, direction) {
+        this.element = element;
+        this.boundingData = element.getBoundingClientRect();
+        this.isMove = false;
+        this.direction = direction;
+
+        this.currentPosition = 0;
+
+        this.initialCoord = {
+            positionStart: 0,
+            clientXStart: 0,
+        };
+
+
+        this.element.addEventListener('mousedown', (e) => {
+            this.isMove = true;
+            this.updateBounding();
+            
+            this.updateCurrentCoord();
+            this.initialCoord.clientXStart = e.clientX;
+
+            activeList.push(this);
+        })
+    }
+
+    updateBounding() {
+        this.boundingData = this.element.getBoundingClientRect();
+    }
+
+    onMove() {
+        this.isMove = true;
+    }
+
+    onStop() {
+        this.isMove = false;
+    }
+
+    updateCurrentCoord() {
+        this.initialCoord.positionStart = parseFloat(this.element.style[this.direction]) || 0;
+    }
+
+    updatePosition(clientX, otherSlider) {
+        const { width } = this.boundingData;
+        const { positionStart, clientXStart } = this.initialCoord;
+        
+        let coord;
+        const maxValue = trackRect.width - (otherSlider.initialCoord.positionStart + width * 2);
+
+        if (this.direction === 'left') {
+            const currentValue = positionStart + (clientX - clientXStart);
+            coord = Math.max(0, Math.min(currentValue, maxValue));
+        } else {
+            const currentValue = positionStart + (-clientX + clientXStart);
+            coord = Math.max(0, Math.min(currentValue, maxValue)); 
+        }
+
+        this.element.style[this.direction] = `${coord}px`;
+        this.currentPosition = coord;
+    }
+
+}
+
+
+
+const sliderLeft = new Slider(left, 'left');
+const sliderRight = new Slider(right, 'right');
+
+const rangeSlider = new RangeSlider(5000, track, sliderLeft, sliderRight);
+rangeSlider.init();
+
+function onMove(e) {
+    const slider = activeList[0];
+
+    if (!(slider && slider.isMove)) return;
+
+    const otherSlider = sliderLeft === slider ? sliderRight : sliderLeft;
+
+    slider.updatePosition(e.clientX, otherSlider);
+    rangeSlider.update();
+}
+
+
+function onForbidden() {
+    if (!activeList[0]) return;
+
+    activeList[0].onStop();
+    activeList[0].updateCurrentCoord();
+    activeList.pop();
+}
+
+document.addEventListener('mouseup', onForbidden);
+document.addEventListener('mousemove', onMove);
---
+++ js/index.js
@@ -0,0 +1,163 @@
+// Write us
+
+function getElementBySelector(className, parent = document) {
+    return parent.querySelector(className);
+}
+
+function getElementsBySelector(className, parent = document) {
+    return [...parent.querySelectorAll(className)];
+}
+
+const btnWriteUs = getElementBySelector('button.about-site__link-button');
+const writeUsModule = getElementBySelector('.write-us');
+const aboutSiteModule = getElementBySelector('.about-site');
+const btnCloseWriteUs = getElementBySelector('.write-us__button-close');
+const btnCloseMap = getElementBySelector('.about-site__button-close');
+const map = getElementBySelector('.about-site__map');
+const writeUsNameField = getElementBySelector('.write-us__input[name=name]');
+const writeUsEmailField = getElementBySelector('.write-us__input[name=email]');
+const writeUsForm = getElementBySelector('.write-us__form');
+
+
+const listeners = [
+    {
+        evt: 'click',
+        mainElement: writeUsModule,
+        elementForOpen: btnWriteUs,
+        elementForClose: btnCloseWriteUs,
+        className: 'write-us--visible'
+    },
+    {
+        evt: 'click',
+        mainElement: aboutSiteModule,
+        elementForOpen: map,
+        elementForClose: btnCloseMap,
+        className: 'about-site--active'
+    },
+];
+
+listeners.forEach(listener => {
+    const {
+        evt,
+        mainElement,
+        elementForOpen,
+        elementForClose,
+        className, 
+    } = listener;
+
+    elementForOpen.addEventListener(evt, () => {
+        if (!mainElement.classList.contains(className)) {
+            mainElement.classList.add(className);
+        }
+    });
+
+    elementForClose.addEventListener(evt, () => {
+        if (mainElement.classList.contains(className)) {
+            mainElement.classList.remove(className);
+            mainElement.classList.remove('module-show');
+            mainElement.classList.remove('module-error');
+        }
+    });
+})
+
+btnWriteUs.addEventListener('click', (e) => {
+    writeUsModule.classList.add('module-show');
+    setTimeout(() => {
+        writeUsModule.classList.remove('module-show');
+    }, 600);
+
+    writeUsNameField.focus();
+    if (localStorage && localStorage.email) {
+        writeUsEmailField.value = localStorage.email;
+    }
+})
+
+function clearForm() {
+    const fields = [...getElementsBySelector('input', writeUsForm), ...getElementsBySelector('textarea', writeUsForm)];
+    fields.forEach(field => field.value = '');
+}
+
+writeUsForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const fieldsData = [...formData.entries()];
+
+    if ( fieldsData.some(([k, value]) => !value) ) {
+        writeUsModule.classList.add('module-error');
+        setTimeout(() => {
+            writeUsModule.classList.remove('module-error');
+        }, 600);
+    } else {
+        localStorage.email = formData.get('email');
+        console.log(`sended: ${fieldsData.reduce((memo, [k, v]) => memo + `${k} - ${v}, `, '').trim()}`)
+        clearForm();
+    }
+})
+
+// Carousel
+
+
+function goodsSlider() {
+    function removeClassItems(list, className) {
+        list.forEach(item => {
+            if (item.classList.contains(className)) {
+                item.classList.remove(className);
+            }
+        })
+    }
+
+    const sliderItems = getElementsBySelector('.slider__list .slider__item');
+    
+    sliderItems.forEach((sliderItem) => {
+        const carouselItems = getElementsBySelector('.carousel__item', sliderItem);
+    
+        carouselItems.forEach((carouselItem, carouselItemIndex) => {
+            carouselItem.addEventListener('click', (e) => {
+                if (carouselItem.classList.contains('carousel__item--active')) return;
+        
+                const activeSliderItem = getElementBySelector('.slider__item--active');
+                activeSliderItem.classList.remove('slider__item--active');
+                activeSliderItem.classList.remove('module-show');
+    
+                const nextActiveSliderItem = sliderItems[carouselItemIndex];
+                nextActiveSliderItem.classList.add('slider__item--active');
+                nextActiveSliderItem.classList.add('module-show');
+    
+                const carouselItems = getElementsBySelector('.carousel__item', nextActiveSliderItem);
+                removeClassItems(carouselItems, 'carousel__item--active');
+                carouselItems[carouselItemIndex].classList.add('carousel__item--active');
+            });
+        });
+    });
+}
+
+goodsSlider();
+
+// Services slider
+
+function servicesSlider() {
+    function removeClassItems(className, parent = document) {
+        const items = getElementsBySelector(`.${className}`, parent);
+        items.forEach(item => item.classList.remove(className));
+    }
+
+    const services = getElementBySelector('.services');
+    const serviceItems = getElementsBySelector('.services__list .services__item');
+    const contentItems = getElementsBySelector('.services__content-list .services__content-item');
+
+    serviceItems.forEach((serviceItem, index) => {
+
+            serviceItem.addEventListener('click', (e) => {
+                if (serviceItem.classList.contains('services__item--current')) return;
+        
+                removeClassItems('services__item--current');
+                removeClassItems('services__content-item--active');
+                removeClassItems('module-show', services);
+
+                serviceItem.classList.add('services__item--current');
+                contentItems[index].classList.add('services__content-item--active', 'module-show')
+            });
+        });
+}
+
+servicesSlider();
